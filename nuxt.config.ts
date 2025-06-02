173da5cc5205ca6daa998b08eaabfa168563e3c4
Mohsen Pakfetrat <mohsenpk1370@gmail.com>
2025-06-02 19:12:29 +0330

    🔥 refactor(nuxt.config.ts): remove file path imports and tailwindcss configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const currentDir = dirname(fileURLToPath(import.meta.url));
-
 export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
@@ -24,10 +19,6 @@ export default defineNuxtConfig({
     '@nuxtjs/device',
     '@nuxt/fonts',
   ],
-  tailwindcss: {
-    cssPath: join(currentDir, './assets/css/tailwind.css'),
-    configPath: join(currentDir, './tailwind.config.js'),
-  },
   i18n: {
     langDir: './lang',
     defaultLocale: 'en',
